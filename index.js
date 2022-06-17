9b625fd06e2db8d74e7abdacc2b71bfec254191e
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-17 14:00:21 +0300

    commit tree 32ed7fc4f453d00bbf5fd3c04d6c1ca608a0453e

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
+function reverseString(word) {
+  //create an array of input strings
+  // let splitWord = word.split("");
+
+  // //reverse the array
+  // let reversedWordArray = splitWord.reverse();
+
+  // //create a reversed string from the reversed array
+  // let reversedWord = reversedWordArray.join("");
+
+  // return reversedWord;
+
+  //shorthand
+  //==========
+  return word.split("").reverse().join("");
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = reverseString(word);
+
+  // return word === reversedWord;
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  ========================
+  reverse input string
+
+  if reversed string is equivalent to input
+    return true
+  else 
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  ==============================================
+  1. Split the word argument into individual letters. Using <split("")> will return an array.
+  2. Reverse the array using the reverse bult-in function. <reverse()>
+  3. Join the letters in the array into a string using the bult-in join function. <join("")>
+  4. Using strict equality, compare original and reversed words. If equal, return true; else return false.
 */
 
 // You can run `node index.js` to view these console logs
